0315c4be6ffaaf2a989865aca2da51b21093c305
bwk73 <bwkersz@gmail.com>
2023-09-08 06:21:20 -0300

    Form handling

--- src/components/FilterCard/index.tsx
+++ src/components/FilterCard/index.tsx
@@ -1,18 +1,51 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState } from "react";
 import "./styles.css";
 
+type FormData = {
+  min?: number;
+  max?: number;
+}
+
 export default function FilterCard() {
+
+  const [formData, setFormData] = useState<FormData>({});
+
+  function handleInputChange(event: any) {
+    const value = event.target.value;
+    const name = event.target.name;
+    setFormData({...formData, [name]: value});
+  }
   
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    console.log(formData.min || 0);
+    console.log(formData.max || Number.MAX_VALUE);
+  }
+
   return (
     <div className="dsf-fc-container">
-      <form className="dsf-fc-content">
+      <form onSubmit={handleSubmit} className="dsf-fc-content">
         <div className="dsf-mt20">
-          <input type="text" placeholder="Preço mínimo" />
+          <input 
+            name="min"
+            value={formData.min || ""}
+            type="text" 
+            placeholder="Preço mínimo" 
+            onChange={handleInputChange}
+          />
         </div>
         <div className="dsf-mt20">
-          <input type="text" placeholder="Preço máximo" />
+          <input
+            name="max" 
+            value={formData.max || ""}
+            type="text" 
+            placeholder="Preço máximo" 
+            onChange={handleInputChange}
+          />
         </div>
         <div className="dsf-mt20">
-          <button>Filtrar</button>
+          <button type="submit">Filtrar</button>
         </div>
       </form>
     </div>
